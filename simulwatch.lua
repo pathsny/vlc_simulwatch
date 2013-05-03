b2aef2b9a632d01103ada6947b5bba7b42898069
Vishnu S Iyengar <pathsny@gmail.com>
2013-05-03 14:48:14 -0700

    added a button and shit

--- simulwatch.lua
+++ simulwatch.lua
@@ -1,21 +1,25 @@
 -- Extension description
 function descriptor()
- return { title = "Remember position" ;
+ return { title = "Simulwatch" ;
   version = "1.0" ;
-  author = "August" ;
-  url = 'http://www.opensubtitles.org/';
-  shortdesc = "Remember file pos";
-  description = "Remember file position.\n"
-    .. "Maintains a database of current play position for all files played." ;
+  author = "Vishnu & Dhruv" ;
+  url = 'https://github.com/pathsny/vlc_simulwatch';
+  shortdesc = "simulwatch";
+  description = "simulwatch" ;
   capabilities = { "input-listener" }
  }
 end
 
 function activate()
    vlc.msg.dbg("[remember pos] Welcome")
-   local d = vlc.dialog( "My VLC Extension" )
-   d:show()
+   local dlg = vlc.dialog("Streaming Radio Player")
+   local button_play = dlg:add_button("Play", click_play, 1, 4, 4, 1)
+   dlg:show()
+   local input = vlc.object.input()
+   vlc.var.add_callback(input, "intf-event", input_event_handler, "Hello world!")
+end
 
+function click_play()
    local fd = vlc.net.connect_tcp( "172.19.23.42", 1377 )
    local data = "Vishnu"
    vlc.msg.dbg(fd);
@@ -27,11 +31,11 @@ function activate()
       data = vlc.net.recv(fd, 10)
       vlc.msg.dbg(data);
 	end
+end
    -- vlc.msg.dbg(vlc.osd.register_channel());
    -- local data = "dhruv"
    -- vlc.osd.message(data, vlc.osd.register_channel(), "bottom", 2000);
-  
-end
+
 
 function deactivate()
  vlc.msg.dbg("[remember pos] deact")
